0e7e9d2d501a348108e3dd6a016978a935a2aa24
Jussi Kajalin <jkajalin@gmail.com>
2023-03-23 17:05:28 +0200

    13.5

---
+++ util/db.js
@@ -0,0 +1,19 @@
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const { DATABASE_URL } = require('./config')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('database connected')
+  } catch (err) {
+    console.log('connecting database failed')
+    return process.exit(1)
+  }
+
+  return null
+}
+
+module.exports = { connectToDatabase, sequelize }
