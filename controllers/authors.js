a6d67f5924c86b41e6444efae8041832f2085176
Jussi Kajalin <jkajalin@gmail.com>
2023-03-30 20:58:28 +0300

    13.16 - authors reitti lisätty

---
+++ controllers/authors.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const { Op, Sequelize } = require('sequelize')
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => { 
+
+  const blogs = await Blog.findAll({ 
+    group: 'author',
+    attributes: [
+      'author',
+      [Sequelize.fn('COUNT', Sequelize.col('author')), 'blogs'],
+      [Sequelize.fn('sum', Sequelize.col('likes')), 'likes']      
+    ],
+    order: [ ['likes', 'DESC'] ]    
+  })
+  // Tulostetaan blogit näkyville consoliin
+  console.log(JSON.stringify(blogs, null, 2))
+
+  res.json(blogs)
+})
+
+module.exports = router
+
+/*
+attributes: [
+      'author',
+      //[Sequelize.fn('COUNT', Sequelize.col('author')), 'n_author'],
+      'likes'
+    ]
+
+*/
